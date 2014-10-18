2785ab3d556a003cec10adba64b99b5b2de1f094
perla <perla11@ru.is>
2014-10-18 01:36:48 +0000

    Main uppfyllir different delimiter test

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -5,31 +5,31 @@ public class Calculator {
 	public static int add(String text){
 		if(text.equals("")){
 			return 0;
+
 		}
-		else if(text.contains(",") || text.contains("\n")){
-			return sum(splitNumbers(text));
-		}
-		else
-			return 1;
+     
+       		 else{
+       		 return splittastreng(text);
+       		 }
 	}
-
-	private static int toInt(String number){
-		return Integer.parseInt(number);
-	}
-
-	private static String[] splitNumbers(String numbers){
-	    numbers = numbers.replace("\n", ",");
-	    return numbers.split(",");
-	}
-      
-    private static int sum(String[] numbers){
- 	    int total = 0;
-        for(String number : numbers){
-		    total += toInt(number);
-		}
-		return total;
-    }
-
-
+   	private static int splittastreng(String text){
+                String[] strengur;
+                String a = text;
+                if(a.contains("//")){
+                        String skipta = Character.toString(a.charAt(2));
+                        a = a.replace(skipta, ",").trim();
+                        a = a.replace("/", ",").trim();
+                        a = a.replace(",", " ").trim();
+                        a = a.replace(" ", ",").trim();
+               		 }
+                a = a.replace("\n", ",");
+                strengur = a.split(",");
+        
+                int summa = 0;
+                for(int i = 0; i < strengur.length; i++){
+                        summa += Integer.parseInt(strengur[i].trim());
+                }
+                return summa;
+        }
 
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -30,8 +30,12 @@ public class CalculatorTest {
    	}
 
 	@Test
-	public void testNewLine (){
+	public void testNewLine(){
 		assertEquals(6, Calculator.add("1\n2,3"));
 	}
 
+	@Test
+	public void testDifferentDelimiter(){
+		assertEquals(3, Calculator.add("//;\n1;2"));
+	}
 }
